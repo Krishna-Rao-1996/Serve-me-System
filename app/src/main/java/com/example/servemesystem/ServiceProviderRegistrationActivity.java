18b0893189c7d2035102ebf127eb066906e3c40c
RaoKrishna <krishnaraoofficial@gmail.com>
2020-02-10 23:21:03 -0600

    Adding a new Activity called ServiceProviderRegistrationActivity with corresponding Layout

---
+++ app/src/main/java/com/example/servemesystem/ServiceProviderRegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.example.servemesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServiceProviderRegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_provider_registration);
+    }
+}
